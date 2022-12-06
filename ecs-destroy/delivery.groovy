9ae4da6b06e237bf2dab9bf90dcf96fbbaab01a2
Lucas Souza Bilu Reis <lsreis@pagseguro.com>
2022-12-06 00:52:59 -0300

    Creating destroy pipeline

---
+++ ecs-destroy/delivery.groovy
@@ -0,0 +1,39 @@
+node('terraform'){
+    cleanWs()
+    def terraform = "${tool 'terraform'}/terraform"
+    def modulesPath = "infrastructure/modules/services/ecs-ec2-backend-app/"
+
+
+    stage(name: 'Checkout'){
+        echo 'Cloning repository'
+        checkoutFromGit("$PARAM_APP_BRANCH")
+    }
+    
+    stage(name: 'Prepare Environment'){
+        executeShCommand("cp -R applications/$PARAM_APP_NAME/devops/ecs/${PARAM_ENVIRONMENT.toLowerCase()}/* $modulesPath ")
+    }
+
+    def credentials = "aws-tdc-qa"
+
+    stage(name: 'Destroy'){
+        timeout(time: 10, unit: "MINUTES"){
+            dir(modulesPath){
+                executeShCommand('terraform  init -no-color -backend-config=backend.conf')
+                executeShCommand("terraform apply --destroy -no-color -auto-approve -var image_version=destroy")
+            }
+        }
+    }
+}
+
+def executeShCommand(command){
+    echo "Execute SH Command: $command"
+    sh command
+}
+
+def checkoutFromGit(branch) {
+    checkout([$class           : 'GitSCM',
+              branches         : [[name: "*/$branch"]],
+              userRemoteConfigs: scm.userRemoteConfigs])
+    sh "git checkout $branch"
+    sh "chmod 777 -R ."
+}
